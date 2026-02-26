674f8e2b78f148fd694283b619a597f6ff87cea4
tigerkrittaphas <krittaphas.chai@gmail.com>
2026-02-25 22:37:53 -0500

    fix deploy problem

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<nav>
+	<a href={`${base}/`}>Home</a>
+	<a href={`${base}/projects/`}>Projects</a>
+	<a href={`${base}/resume/`}>Resume</a>
+	<a href={`${base}/contact/`}>Contact</a>
+	<a href="https://github.com/tigerkrittaphas" target="_blank" rel="noopener noreferrer">GitHub</a>
+</nav>
+
+<slot />
